e3bdf782948090b10cc36d5e6cd17491787d545c
Benjamin <benjiprores@gmail.com>
2025-02-18 23:43:05 +0000

    props, usestate and useeffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
+import {useEffect, useState} from "react";
+
 const Card = ({title}) => {
+
+    const [count, setCount] = useState(0)
+    const [hasLiked, setHasLiked] = useState(false)
+
+    useEffect(() => {
+        console.log(`${title} has been liked: ${hasLiked}`);
+    }, [hasLiked])
+
     return (
-        <h2>{title}</h2>
+        <div className="card" onClick={() => setCount((prevState) => prevState + 1)}>
+            <h2>{title} <br/> {count ? count : null}</h2>
+
+             <button onClick={() => setHasLiked(!hasLiked)}>
+                {hasLiked ? 'L iked' : 'Like'}
+            </button>
+        </div>
     )
 }
 
 const App = () => {
+
     return (
         <>
-            <div>Functional Arrow Component</div>
-            <Card title="Star War" subTitle="Wars at next level"/>
-            <Card title="Avatar"/>
-            <Card title="Lion King"/>
+            <div className="container">
+                <Card title="Star War" subTitle="Wars at next level" rating={5}/>
+                <Card title="Avatar"/>
+                <Card title="Lion King"/>
+            </div>
         </>
     )
 }
